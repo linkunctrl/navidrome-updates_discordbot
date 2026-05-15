5a8cd8dd849676bb9cc36a492438b0a52c86401a
lin <137373266+linkunctrl@users.noreply.github.com>
2026-05-15 19:46:17 +0500

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,11 +1,8 @@
-
 import subprocess
 import requests
-import os
-
-WEBHOOK_URL = "https://discord.com/api/webhooks/1504796089184813198/_Qgx3EiDWKwPhpxysTK-TLbWa_7bP42dWRwjQWLcdHGGckGC2wNqQqs2iv4xKGQvkste"
-CONTAINER_NAME = "navidrome"
-STATE_FILE = "/tmp/navidrome_last_state"
+ 
+WEBHOOK_URL = "add your url here"
+CONTAINER_NAME = "navidrome"  
 
 def get_status():
     result = subprocess.run(
@@ -15,18 +12,7 @@ def get_status():
     return result.stdout.strip() == "running"
 
 def notify(is_running):
-    msg = ":3 zee moosik server is **UP**" if is_running else ":p zee moosik server is **DOWN**"
+    msg = ":) Navidrome is **UP**" if is_running else ":( Navidrome is **DOWN**"
     requests.post(WEBHOOK_URL, json={"content": msg})
 
-current = get_status()
-
-if os.path.exists(STATE_FILE):
-    with open(STATE_FILE) as f:
-        last = f.read().strip() == "True"
-    if current != last:
-        notify(current)
-else:
-    notify(current)
-
-with open(STATE_FILE, "w") as f:
-    f.write(str(current))
+notify(get_status())
